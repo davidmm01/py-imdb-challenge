e43d8bc63883a97a586059493586857a6828117b
David Michieli <d.m.michieli@gmail.com>
2019-11-23 19:56:38 +1100

    part 1 WIP

--- README.md
+++ README.md
@@ -1,1 +1,29 @@
 # py-imdb-challenge
+
+A programming challenge to retrieve the films that actors have appeared in from imdb
+
+# Usage
+To do
+
+# What's the source of the data?
+
+There is no official imdb API.  Some alternatives exist (a few on rapidapi.com, omdb, themoviedb), but these would require users to sign up and grab an API key which is a pain (and they also don't meet the spec of getting the data FROM imdb).  Instead this tool directly queries the imdb website and cleans up the returned HTML.  
+
+This will break if the webpage changes.  
+
+# For Developers
+
+## Environment
+Use a virtual environment when developing this project
+
+`python3 -m venv venv`
+
+`. ./venv/bin/activate`
+
+## Updating Requirements
+
+This project makes use of pip-tool's `pip-compile` tool for managing its requirements.  To update, please run
+
+`pip-compile`
+
+`pip install -rrequirements.txt`
---
+++ actors_films.py
@@ -0,0 +1,125 @@
+import argparse
+from bs4 import BeautifulSoup
+import requests
+
+
+class Actor:
+    def __init__(self, name, slug, example_job=None, example_film=None):
+        self.name = name
+        self.slug = slug
+        self.example_job = example_job
+        self.example_film = example_film
+
+
+class Movie:
+    def __init__(self, title, year):
+        self.title = title
+        self.year = year
+
+
+# CMD LINE ARGS
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "actor", help="name of actor for which films will be displayed", type=str
+)
+arguments = parser.parse_args()
+actor_search_term = arguments.actor
+
+# s=nm query param searches for celebs
+response = requests.get(f"https://www.imdb.com/find?s=nm&q={actor_search_term}&ref_=nv_sr_sm")
+
+bs = BeautifulSoup(response.text, features="html.parser")
+
+# only grab result text, not the accompanying "primary_photo"
+table_rows = bs.find_all('td', class_="result_text")
+
+actors = []
+
+for row in table_rows:
+
+    # do a case-insensitive match
+    if row.a.string.lower() == actor_search_term.lower():
+
+        # all returned results have a name and slug
+        name = row.a.string
+        slug = row.a["href"]
+
+        # some returned results will not have an example work of the actor
+        if row.small:
+            try:
+                example_job = row.small.contents[0].strip("(").strip(", ")
+                example_film = (row.small.contents[1].string)
+            except KeyError:
+                # this except might not be required, but keeping for defense
+                example_job = None
+                example_film = None
+        else:
+            example_job = None
+            example_film = None
+
+        actors.append(Actor(name, slug, example_job, example_film))
+
+
+actor_matches = len(actors)
+
+
+if actor_matches == 0:
+    print(f"No actors found named {actor_search_term}")
+    exit
+elif actor_matches > 1:
+    print(f"Found {actor_matches} actors with name {actor_search_term}")
+    print("Please select one actor by entering the corresponding number:\n")
+
+    count = 1
+    for actor in actors:
+        print(f"{count}) {actor.name}")
+        if actor.example_job is not None and actor.example_film is not None:
+            print(f"   Known as")
+            print(f"   {actor.example_job} in {actor.example_film}")
+        count += 1
+        print("")
+    while True:
+        selection = input("Selection: ")
+        try:
+            selection = int(selection) - 1
+            actor = actors[selection]
+        except ValueError:
+            print("Ensure your selection is a number")
+        except IndexError:
+            print(f"Ensure your selection is between 1 and {actor_matches}")
+        else:
+            break
+else:
+    actor = actors[0]
+
+# TODO: decide if we actually need this #actor on the slug, prolly not
+response = requests.get(f"https://www.imdb.com{actor.slug}#actor")
+
+# the returned HTML from the page is not nicely broken into a parent-children
+# structure for the lists of films under the actor/producer/... sections
+# so we forceably cut out everything before and after the actor tag before 
+# putting it into beautiful soup
+start_cut_index = response.text.find('<a name="actor">Actor</a>')
+trimmed_response = response.text[start_cut_index:]
+# skip the first 8 chars, since they already match
+end_cut_index = trimmed_response[8:].find('<a name="')
+trimmed_response = trimmed_response[0:end_cut_index]
+
+
+bs = BeautifulSoup(trimmed_response, features="html.parser")
+
+movies = []
+for movie in bs.find_all(class_="filmo-row"):
+    title = movie.a.string
+    year = movie.span.string.strip()
+
+    if len(year) == 0:
+        year = "Upcoming"
+
+    movies.append(Movie(title, year))
+
+
+count = 1
+for movie in movies:  
+    print(f"{count}) {movie.title}, {movie.year}")
+    count += 1
